09ae60a90e3d88740ba988e9d124218ee1095d77
Thiago Linchin <dev.thiagolinch@gmail.com>
2026-04-16 17:53:01 -0400

    fix: update database connection details and environment variables

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -11,7 +11,7 @@ interface IOptions {
 
 getConnectionOptions().then(options => {
   const newOptions = options as IOptions;
-  newOptions.host = 'db.dlfibacmlqphqkajpscz.supabase.co';
+  newOptions.host = 'db.ofquxucuvinakriqhuzg.supabase.co';
 
   createConnection({
     ...options,
